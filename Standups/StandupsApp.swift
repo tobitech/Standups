049e70ce4ca54235768fe1c021b3c65ca547b78d
Tobi Omotayo <tobitech@ymail.com>
2023-01-24 10:47:37 +0100

    Initial Commit

---
+++ Standups/StandupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandupsApp.swift
+//  Standups
+//
+//  Created by Oluwatobi Omotayo on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct StandupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
