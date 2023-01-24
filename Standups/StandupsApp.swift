0bb98efdc20bc1911c1af0b2488d716279eed699
Tobi Omotayo <tobitech@ymail.com>
2023-01-24 10:48:48 +0100

    project setup

--- Standups/StandupsApp.swift
+++ Standups/StandupsApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct StandupsApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
